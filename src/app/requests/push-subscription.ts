ca333cc533f4aa9d8469568b2fc630913d930ec3
Alexey Smirnov <alexey.smirnov@muranosoft.com>
2018-10-27 16:07:05 +0300

    fix

--- src/app/requests/push-subscription.ts
+++ src/app/requests/push-subscription.ts
@@ -1,8 +1,8 @@
 export class PushSubscription {
-  endpoint: string,
-  expirationTime: string | null,
+  endpoint: string;
+  expirationTime: string | null;
   keys: {
     p256dh: string,
     auth: string
-  }
+  };
 }
