1cb298e8c4a930e2032f05eb911b18705b91a19c
Alexey Smirnov <alexey.smirnov@muranosoft.com>
2018-10-27 06:38:58 +0300

    timer

--- src/app/components/start-card/start-card.component.ts
+++ src/app/components/start-card/start-card.component.ts
@@ -14,13 +14,10 @@ import { Session } from 'src/app/models/Session';
   templateUrl: './start-card.component.html',
   styleUrls: ['./start-card.component.scss']
 })
-export class StartCardComponent implements OnInit, OnDestroy {
+export class StartCardComponent implements OnInit {
 
   private exercises = '/exercises';
   private nextSession: UserData;
-  private counter$: Observable<number>;
-  private subscription: Subscription;
-  private message: string;
   private session: Session;
   count = 0;
   constructor(private scheduler: SchedulerService, private exercise: ExercisesService, private router: Router) { }
@@ -42,54 +39,9 @@ export class StartCardComponent implements OnInit, OnDestroy {
     this.exercise.getCurrentSession(new ReqExercises(this.nextSession.userId))
       .subscribe(x => {
         this.session = x;
-      }).add(x => this.runCounter());
+      });
   }
-
-  private runCounter() {
-    this.count = 3600;
-    const date = new Date();
-    if (this.session) {
-      this.count -= (date.getMinutes() * 60 + date.getSeconds());
-    }
-    this.counter$ = interval(1000).pipe(map((x) => {
-      if (this.session) {
-        return this.count -= 1;
-      } else {
-        this.count = 0;
-        return this.count;
-      }
-    }));
-    this.subscription = this.counter$.subscribe((x) => {
-      this.message = this.dhms(x);
-      if (x === 0) {
-        this.subscription.unsubscribe();
-        this.redirectToExec();
-      }
-    });
-  }
-
   redirectToExec() {
     this.router.navigate([this.exercises]);
-  }
-  dhms(x: number): string {
-    const watch = [
-      (x - x % (60 * 60)) / (60 * 60),
-      (x % (60 * 60) - x % 60) / 60,
-      x % (60)];
-    let retMessage = '';
-    watch.forEach((element, i) => {
-      if (element !== 0) {
-        if (i !== 0 && watch[i - 1] !== 0) {
-          retMessage += ' : ';
-        }
-        retMessage += element.toString();
-      }
-    });
-    return retMessage;
-  }
-  ngOnDestroy(): void {
-    if (this.subscription && this.subscription.unsubscribe) {
-      this.subscription.unsubscribe();
-    }
   }
 }
